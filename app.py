2b662720db0e28e81ce5a311045cf142c5ca6b4d
jmungapara <janvimungapara2@gmail.com>
2024-12-12 09:22:33 +0530

    add port

--- app.py
+++ app.py
@@ -46,4 +46,5 @@ def upload_image():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port = 10000)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(debug=True, host='0.0.0.0', port=port)
